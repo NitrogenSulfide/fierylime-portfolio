75d3c0e5752d18a8be4ecba8b049f285b5013a7c
Oliver Luo <32621227+loadingthecode@users.noreply.github.com>
2025-12-13 15:35:21 -0500

    BE-2: Add Spring Web and health check endpoint

---
+++ backend/src/main/java/io/github/nitrogensulfide/portfolio/backend/FierylimePortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.nitrogensulfide.portfolio.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FierylimePortfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FierylimePortfolioBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/nitrogensulfide/portfolio/backend/FierylimePortfolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nitrogensulfide.portfolio.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FierylimePortfolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
